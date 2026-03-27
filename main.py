082c2fe3921f2293f26ebc9431f50dfdc508f413
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-27 19:25:14 +0100

    Add History, POST request Endpoints

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI
-from analyzer import analyze_job
-from analyzer import analyze_jobFake
-
+from analyzer import analyze_job, analyze_jobFake
+from pydantic import BaseModel
+import pydantic
 app = FastAPI()
+history = []
+
+class JobRequest(BaseModel):
+    title: str
+    company:str
+
+@app.get("/history")
+def get_history():
+    return history
 
 @app.get('/')
 def home():
     return {'message': 'AI Job Analyzer API is running'}
 
-# Analyze using predefined data
+# POST Analysis
+@app.post("/analyze")
+def analyze_post(job: JobRequest):
+    result = analyze_jobFake(job.model_dump())
+
+    history.append({
+        "job": job.model_dump(),
+        "result": result
+    })
+    return {"analysis": result}
+
+# GET Analysis
 @app.get("/analyze")
 def analyze(title: str, company: str):
 
