c5749e4ab0d891d2064e9f5f1b51778605982eb9
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-27 18:37:14 +0100

    Add analysis endpoint

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
-from analyzer import analyzer
+from analyzer import analyze_job
+from analyzer import analyze_jobFake
 
 app = FastAPI()
 
 @app.get('/')
 def home():
     return {'message': 'AI Job Analyzer API is running'}
+
+# Analyze using predefined data
+@app.get("/analyze")
+def analyze(title: str, company: str):
+
+    job = {
+        "title": title,
+        "company": company
+    }
+
+    result = analyze_jobFake(job)
+
+    return{
+        "analysis": result
+    }
+
+# Analyze using AI
+@app.get("/analyzeAI")
+def analyze(title: str, company: str):
+
+    job = {
+        "title": title,
+        "company": company
+    }
+
+    result = analyze_job(job)
+
+    return{
+        "analysis": result
+    }
